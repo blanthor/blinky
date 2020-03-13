d58a20e607c1afa1f9acfe46f0a86ffea277340a
blanthor <blanthor@users.noreply.github.com>
2020-03-13 18:52:40 -0500

    Create blinky.py

---
+++ blinky.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+LedPin = 11 #GPIO pin number 11 on the Pi
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(LedPin, GPIO.OUT)
+    GPIO.output(LedPin, GPIO.HIGH)
+s
+def loop():
+    while True:
+        print ('´...led on´')
+        GPIO.output(LedPin, GPIO.LOW)
+        time.sleep(1.5)
+        print ('´...led off´')
+        GPIO.output(LedPin, GPIO.HIGH)
+        time.sleep(0.5)
+
+def destroy():
+        GPIO.output(LedPin, GPIO.HIGH)
+        GPIO.cleanup()
+
+if __name__ == '__main__':
+        setup()
+        try:
+            loop()
+        except KeyboardInterrupt:
+            destroy()
